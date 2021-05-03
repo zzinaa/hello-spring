c149b140b52fbb05beddfe86fd4fe7f58db22e6b
zzinaa <wlsdk2769@ajou.ac.kr>
2021-05-04 00:03:44 +0900

    Add MemberService class

---
+++ src/main/java/com/zzinaa/hellospring/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.zzinaa.hellospring.service;
+
+import com.zzinaa.hellospring.domain.Member;
+import com.zzinaa.hellospring.repository.MemberRepository;
+import com.zzinaa.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    //회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    //아이디로 회원 조회
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}

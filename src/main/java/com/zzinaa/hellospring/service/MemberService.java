c263f6554a0314cbe0cc9bb4c29902cc57b2f9aa
zzinaa <wlsdk2769@ajou.ac.kr>
2021-05-05 12:04:39 +0900

    Add annotation for component scan

--- src/main/java/com/zzinaa/hellospring/service/MemberService.java
+++ src/main/java/com/zzinaa/hellospring/service/MemberService.java
@@ -3,14 +3,18 @@ package com.zzinaa.hellospring.service;
 import com.zzinaa.hellospring.domain.Member;
 import com.zzinaa.hellospring.repository.MemberRepository;
 import com.zzinaa.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
